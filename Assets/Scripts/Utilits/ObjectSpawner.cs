8dbc52908a1124f71cffc9acef82b37449bb8dc8
Hypnos <47752595+Terryhata6@users.noreply.github.com>
2021-04-06 12:49:46 +0300

    Commits

--- Assets/Scripts/Utilits/ObjectSpawner.cs
+++ Assets/Scripts/Utilits/ObjectSpawner.cs
@@ -5,18 +5,13 @@ public class ObjectSpawner : MonoBehaviour
 {
     public static ObjectSpawner current;
     [SerializeField]private GameObject _coinObject;
-    // Use this for initialization
+    
     public void Awake()
     {
         DontDestroyOnLoad(this);
         current = this;
     }
 
-    // Update is called once per frame
-    public void Update()
-    {
-
-    }
 
     public GameObject CreateCoin(Vector3 position) => Instantiate(_coinObject, position, Quaternion.identity);
     
