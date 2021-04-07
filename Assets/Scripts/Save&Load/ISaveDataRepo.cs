325eab5671e6cf0228aa57307351c7893d4b115b
Dron Corleone <kefirchik13@gmail.com>
2021-04-07 14:56:09 +0300

    Save&Load

---
+++ Assets/Scripts/Save&Load/ISaveDataRepo.cs
@@ -0,0 +1,10 @@
+﻿public interface ISaveDataRepo
+{
+    void SaveData(int value, string key);
+    void SaveData(float value, string key);
+    void SaveData(string value, string key);
+
+    int LoadInt(string key);
+    float LoadFloat(string key);
+    string LoadString(string key);
+}
