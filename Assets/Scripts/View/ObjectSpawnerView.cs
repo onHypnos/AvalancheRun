1394335ecbed53fc6c7a7f1aa86bcbf4b585854c
Hypnos <47752595+Terryhata6@users.noreply.github.com>
2021-04-09 14:38:24 +0300

    ObjectSpawnersUpdates

--- Assets/Scripts/View/ObjectSpawnerView.cs
+++ Assets/Scripts/View/ObjectSpawnerView.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class ObjectSpawnerView : BaseObjectView
 {
-    [SerializeField] private GameObject[] _array;
-    public void CreateObjects()
+    private ObjectSpawnerController _controller;
+    [SerializeField] private FallingObjectsCSO _objects;
+    public FallingObjectsCSO ObjectList => _objects;
+    private void Awake()
     {
-        if (_array != null)
+        if (_controller != null)
         {
-        
-
+            FindMyController();
         }
+    }
+
+    private void FindMyController()
+    {
+        _controller = FindObjectOfType<MainController>().GetController<ObjectSpawnerController>();        
+        if (_controller != null)
+        {
+            GameEvents.current.SetObjectSpawner(this);
+        }
+    }
+
+    private void OnDestroy()
+    {        
+        GameEvents.current.OnDeleteObjectSpawner(this);
     }
 }
